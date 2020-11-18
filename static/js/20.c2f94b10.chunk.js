54fbd980cb355bdcde925eb88e36aace5bf59a4b
Elliot Kim <elliotkim916@gmail.com>
2020-11-17 19:46:23 -0800

    Updates

---
+++ static/js/20.c2f94b10.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpone-click-test"]=this["webpackJsonpone-click-test"]||[]).push([[20],{356:function(t,e,n){"use strict";n.r(e),n.d(e,"amplify_container",(function(){return i}));var c=n(9),i=function(){function t(t){Object(c.k)(this,t)}return t.prototype.render=function(){return Object(c.i)(c.b,null,Object(c.i)("slot",null))},t}();i.style=":host{display:-ms-flexbox;display:flex;-ms-flex-align:center;align-items:center;-ms-flex-pack:center;justify-content:center;height:100vh}"}}]);
+//# sourceMappingURL=20.c2f94b10.chunk.js.map
